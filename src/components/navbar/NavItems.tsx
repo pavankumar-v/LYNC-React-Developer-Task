ec19a9f5403398b0dc260a5b0cd748efd0387429
pavankumar-v <pavankumarv124@gmail.com>
2024-04-07 18:46:45 +0530

    refactor(navlink): replacing link with NavLink for active style

--- src/components/navbar/NavItems.tsx
+++ src/components/navbar/NavItems.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import Button from '@ui/Button';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavItems: React.FC = () => {
   return (
     <ul className="flex justify-center items-center gap-2">
       <li className="text">
         <Button variant="default">
-          <Link to={'/books'}>Books</Link>
+          <NavLink
+            to={'/books'}
+            className={({ isActive }): string => {
+              return isActive ? 'text-primary' : '';
+            }}
+          >
+            Books
+          </NavLink>
         </Button>
       </li>
       <li>
         <Button variant="default">
-          <Link to={'/bookmarks'}>Bookmarks</Link>
+          <NavLink
+            to={'/bookmarks'}
+            className={({ isActive }): string => {
+              return isActive ? 'text-primary' : '';
+            }}
+          >
+            Bookmarks
+          </NavLink>
         </Button>
       </li>
       <Button>Login</Button>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
     <nav className="sticky top-0 z-50 text-foreground py-3 bg-primary/5">
       <div className="container flex justify-between items-center">
         <div>
-          <Link to={'/books'} className="text-xl text-primary">
+          <Link to={'/books'} className="text-xl">
             Book Store
           </Link>
         </div>
