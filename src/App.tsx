99a25fdcc5d9308c63e3aace9adbf0ae18777efc
pavankumar-v <pavankumarv124@gmail.com>
2024-04-09 23:48:46 +0530

    feat(authctx): auth context to access state globally

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import Books from './components/pages/Books';
 import Bookmarks from './components/pages/Bookmarks';
 import BookInfo from './components/pages/BookInfo';
 import LoginPage from './components/pages/LoginPage';
+import AuthContextProvider from './contexts/AuthContext';
 
 const App: React.FC = () => {
   return (
     <div className="text-primary min-h-screen max-w-screen">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route element={<LoginPage />} path="/auth/login" />
-          <Route element={<Books />} path="/books" />
-          <Route element={<BookInfo />} path="/books/:id" />
-          <Route element={<Bookmarks />} path="/bookmarks" />
-          <Route element={<Books />} path="/cart" />
-          <Route element={<Books />} path="/orders" />
-          {/* <Route element={<></>}> */}
-          {/* </Route> */}
-        </Routes>
+        <AuthContextProvider>
+          <Navbar />
+          <Routes>
+            <Route element={<LoginPage />} path="/auth/login" />
+            <Route element={<Books />} path="/books" />
+            <Route element={<BookInfo />} path="/books/:id" />
+            <Route element={<Bookmarks />} path="/bookmarks" />
+            <Route element={<Books />} path="/cart" />
+            <Route element={<Books />} path="/orders" />
+            {/* <Route element={<></>}> */}
+            {/* </Route> */}
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
