1cac2bc7e299d8fbac32cab3ba00382082c69615
pavankumar-v <pavankumarv124@gmail.com>
2024-04-10 10:26:34 +0530

    feat(authconstaint): creating AuthProtection component and wrapping to routes for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from 'react-router-dom';
 import Books from './components/pages/Books';
 import Bookmarks from './components/pages/Bookmarks';
 import BookInfo from './components/pages/BookInfo';
 import LoginPage from './components/pages/LoginPage';
-import AuthContextProvider from './contexts/AuthContext';
+import AuthContextProvider, { AuthContext } from './contexts/AuthContext';
+import Spinner from './components/ui/Spinner';
 
 const App: React.FC = () => {
   return (
@@ -18,16 +25,31 @@ const App: React.FC = () => {
             <Route element={<LoginPage />} path="/auth/login" />
             <Route element={<Books />} path="/books" />
             <Route element={<BookInfo />} path="/books/:id" />
-            <Route element={<Bookmarks />} path="/bookmarks" />
             <Route element={<Books />} path="/cart" />
             <Route element={<Books />} path="/orders" />
-            {/* <Route element={<></>}> */}
-            {/* </Route> */}
+            <Route element={<AuthProtected />}>
+              <Route element={<Bookmarks />} path="/bookmarks" />
+            </Route>
           </Routes>
         </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
+};
+
+const AuthProtected: React.FC = () => {
+  const { isAuthenticated, isLoading } = useContext(AuthContext);
+  console.log(isAuthenticated);
+
+  if (isLoading) {
+    <Spinner>Loading...</Spinner>;
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <Navigate to={'/auth/login'} />;
 };
 
 export default App;
