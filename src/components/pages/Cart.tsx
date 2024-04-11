17aff2b67164111c47223c968a77839b31970145
pavankumar-v <pavankumarv124@gmail.com>
2024-04-11 09:52:43 +0530

    feat(cart): remove item from cart

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,49 +1,72 @@
 import React, { useContext } from 'react';
 import { BookContext, BookContextType } from '@/contexts/BookContext';
-import { Book } from '@/interface';
+import { CartItem } from '@/interface';
 import CheckOutButton from '../book/CheckOutButton';
+import Button from '../ui/Button';
 
 const Cart: React.FC = () => {
-  const { books, cartItems } = useContext(BookContext) as BookContextType;
-  const cartBooks: Book[] = books.filter((book) =>
-    cartItems.map((cartItem) => cartItem.bookId).includes(book.id)
+  const { books, cartItems, removeFromCart } = useContext(
+    BookContext
+  ) as BookContextType;
+  const cartBooks: CartItem[] = cartItems.reduce(
+    (filteredBooks: CartItem[], cartItem) => {
+      const book = books.find((book) => book.id == cartItem.bookId);
+      if (book) {
+        filteredBooks.push({ ...cartItem, book });
+      }
+      return filteredBooks;
+    },
+    []
   );
 
   const totalPrice: number | undefined = cartBooks
-    .map((book) => book.saleInfo.listPrice?.amount)
-    .reduce((a, b) => (a || 0) + (b || 0), 0);
+    .map((cart) => cart.book?.saleInfo.listPrice?.amount)
+    .reduce((total, bookPrice) => (total || 0) + (bookPrice || 0), 0);
 
   return (
     <>
       <div className="container flex flex-col gap-5 w-full mt-2">
-        {cartBooks.map((book, i) => (
+        {cartBooks.map((cart, i) => (
           <div
-            className="w-full flex gap-4 p-3 border bg-card rounded-xl"
+            className="flex justify-between gap-4 p-3 border bg-card rounded-xl"
             key={i}
           >
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt=""
-              width={50}
-              className="rounded-sm"
-            />
-            <div className="flex flex-col">
-              <h4 className="text-primary text-lg">{book.volumeInfo.title}</h4>
-              <p className="text-sm text-forground">
-                {book.volumeInfo.subtitle}
-              </p>
-              <h2 className="text-md text-accent mt-2">
-                {book.saleInfo.listPrice?.amount}{' '}
-                {book.saleInfo.listPrice?.currencyCode}
-              </h2>
+            <div className="flex gap-3">
+              <img
+                src={cart.book?.volumeInfo.imageLinks.thumbnail}
+                alt=""
+                width={50}
+                className="rounded-sm"
+              />
+              <div className="flex flex-col">
+                <h4 className="text-primary text-lg">
+                  {cart.book?.volumeInfo.title} {cart.id}
+                </h4>
+                <p className="text-sm text-forground">
+                  {cart.book?.volumeInfo.subtitle}
+                </p>
+                <h2 className="text-md text-accent mt-2">
+                  {cart.book?.saleInfo.listPrice?.amount}{' '}
+                  {cart.book?.saleInfo.listPrice?.currencyCode}
+                </h2>
+              </div>
             </div>
+            <Button
+              variant="destructive"
+              className="self-start"
+              onClick={() => {
+                removeFromCart(cart.id);
+              }}
+            >
+              Remove
+            </Button>
           </div>
         ))}
       </div>
       <div className="fixed w-full z-40 bottom-0 bg-accent p-3">
         <div className="container flex justify-between items-center">
           <h2 className="text-lg font-bold">Total: {totalPrice}</h2>
-          <CheckOutButton cartBooks={cartBooks} />
+          <CheckOutButton cartItems={cartBooks} />
         </div>
       </div>
     </>
